e1031a04b40089a99cc573d862764b0b30a899d7
Dzmitry Prokharau <goodmediadeveloper@gmail.com>
2019-03-16 08:23:12 +0300

    fc

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  
+  var zeros = 0;
+  var remainder = 1;
+  var divisor = 2;
+
+  while (base !== divisor) {
+    if (base % divisor === 0) {
+      remainder++;
+      base /= divisor;
+
+    } else {
+      remainder = 1;
+      divisor++;
+    }
+  }
+
+  while (number > 0) {
+    number = number / divisor ^ 0;
+    zeros += number;
+  }
+  return parseInt(zeros / remainder);
 }
